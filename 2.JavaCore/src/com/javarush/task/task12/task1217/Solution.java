44da1273db29eb0a8bd26c98cea44ae223dd973d
ruslan zh <uit@atu.kz>
2022-08-15 16:32:55 +0600

    some task

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly{
+        void speed();
+    }
+    public interface CanRun{
+        void speed();
+    }
+    public interface CanSwim{
+        void speed();
+    }
+//add interfaces here - добавь интерфейсы тут
+
+}
